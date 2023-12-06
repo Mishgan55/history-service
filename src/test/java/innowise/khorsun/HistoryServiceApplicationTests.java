d7288fb0d3243f7b4050fffb2b215cfdc08a8ada
mishgan55 <vorota.mogilev@gmail.com>
2023-12-06 12:35:08 +0300

    Initial commit

---
+++ src/test/java/innowise/khorsun/HistoryServiceApplicationTests.java
@@ -0,0 +1,10 @@
+package innowise.khorsun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
